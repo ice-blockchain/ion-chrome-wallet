20d346d8e0ec6e62aa91224151831cc87e4c09f7
Andrew <andrew@leftclick.io>
2024-12-27 01:29:23 +0200

    1. Changed the `Welcome` logo. 2. Changed the `Welcome` title.

--- src/view/screen/initialize/Initialize.tsx
+++ src/view/screen/initialize/Initialize.tsx
@@ -1,96 +1,90 @@
-import { useState } from "react";
+import {useState} from "react";
 import styled from "styled-components";
 import ExtensionPlatform from "../../../libs/service/extension";
-import {
-  ButtonColumn,
-  ButtonNegative,
-  ButtonPositive,
-  Container,
-  H1,
-} from "../../components/Components";
-import { InputField } from "../../components/InputField";
-import { LoadingLogo } from "../../components/Logo";
-import { AppRoute } from "../../routes";
-import { ConnectRoutes } from "../import/ConnectWallet";
-import { useCreatePasswordMutation } from "./api";
+import {ButtonColumn, ButtonNegative, ButtonPositive, Container, H1,} from "../../components/Components";
+import {InputField} from "../../components/InputField";
+import {LoadingLogo} from "../../components/Logo";
+import {AppRoute} from "../../routes";
+import {ConnectRoutes} from "../import/ConnectWallet";
+import {useCreatePasswordMutation} from "./api";
 
 const Body = styled(Container)`
-  width: 100%;
-  flex-grow: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+    width: 100%;
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 `;
 
 export const Initialize = () => {
-  return (
-    <Body>
-      <LoadingLogo />
-      <H1>Welcome to OpenMask</H1>
-      <ButtonColumn>
-        <ButtonPositive
-          onClick={() =>
-            ExtensionPlatform.openExtensionInBrowser(
-              AppRoute.import + ConnectRoutes.create
-            )
-          }
-        >
-          Create Wallet
-        </ButtonPositive>
-        <ButtonNegative
-          onClick={() =>
-            ExtensionPlatform.openExtensionInBrowser(
-              AppRoute.import + ConnectRoutes.import
-            )
-          }
-        >
-          Import Wallet
-        </ButtonNegative>
-      </ButtonColumn>
-    </Body>
-  );
+    return (
+        <Body>
+            <img src="ion-logo.svg" alt="ION" width={117} height={117}/>
+            <H1>Welcome to ION Wallet</H1>
+            <ButtonColumn>
+                <ButtonPositive
+                    onClick={() =>
+                        ExtensionPlatform.openExtensionInBrowser(
+                            AppRoute.import + ConnectRoutes.create
+                        )
+                    }
+                >
+                    Create Wallet
+                </ButtonPositive>
+                <ButtonNegative
+                    onClick={() =>
+                        ExtensionPlatform.openExtensionInBrowser(
+                            AppRoute.import + ConnectRoutes.import
+                        )
+                    }
+                >
+                    Import Wallet
+                </ButtonNegative>
+            </ButtonColumn>
+        </Body>
+    );
 };
 
 export const CreatePassword = () => {
-  const [password, setPassword] = useState("");
-  const [confirm, setConfirm] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirm, setConfirm] = useState("");
 
-  const { mutateAsync, reset, isLoading, error } = useCreatePasswordMutation();
+    const {mutateAsync, reset, isLoading, error} = useCreatePasswordMutation();
 
-  const onCreate = async () => {
-    reset();
-    await mutateAsync([password, confirm]);
-  };
+    const onCreate = async () => {
+        reset();
+        await mutateAsync([password, confirm]);
+    };
 
-  return (
-    <Body>
-      <LoadingLogo />
-      <H1>Create Password</H1>
-      <ButtonColumn>
-        <div>
-          <InputField
-            label="New password"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
+    return (
+        <Body>
+            <LoadingLogo/>
+            <H1>Create Password</H1>
+            <ButtonColumn>
+                <div>
+                    <InputField
+                        label="New password"
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
 
-        <div>
-          <InputField
-            label="Confirm password"
-            type="password"
-            value={confirm}
-            onChange={(e) => setConfirm(e.target.value)}
-            error={error}
-          />
-        </div>
+                <div>
+                    <InputField
+                        label="Confirm password"
+                        type="password"
+                        value={confirm}
+                        onChange={(e) => setConfirm(e.target.value)}
+                        error={error}
+                    />
+                </div>
 
-        <ButtonPositive onClick={onCreate} disabled={isLoading}>
-          Create
-        </ButtonPositive>
-      </ButtonColumn>
-    </Body>
-  );
+                <ButtonPositive onClick={onCreate} disabled={isLoading}>
+                    Create
+                </ButtonPositive>
+            </ButtonColumn>
+        </Body>
+    );
 };
