6373a20c7fd7e11dfd6bf1e6c6692acc3207b99f
Andrew <andrew@leftclick.io>
2025-01-09 13:01:56 +0200

    1. Centered logos on the `Send` loading / success screens.

--- src/view/components/send/SendSuccessView.tsx
+++ src/view/components/send/SendSuccessView.tsx
@@ -1,60 +1,50 @@
-import { FC } from "react";
-import { useNavigate } from "react-router-dom";
+import {FC} from "react";
+import {useNavigate} from "react-router-dom";
 import ExtensionPlatform from "../../../libs/service/extension";
-import { AppRoute } from "../../routes";
-import { useSelectedNetworkConfig } from "../../screen/home/api";
-import {
-  Body,
-  ButtonColumn,
-  ButtonNegative,
-  ButtonPositive,
-  Center,
-  Gap,
-  H1,
-  Text,
-} from "../Components";
-import { LinkIcon } from "../Icons";
-import { LoadingLogo } from "../Logo";
+import {AppRoute} from "../../routes";
+import {useSelectedNetworkConfig} from "../../screen/home/api";
+import {Body, ButtonColumn, ButtonNegative, ButtonPositive, Center, Gap, H1, Text,} from "../Components";
+import {LinkIcon} from "../Icons";
 
 export interface Props {
-  address: string;
-  homeRoute?: string;
+    address: string;
+    homeRoute?: string;
 }
 
 export const SendSuccessView: FC<Props> = ({
-  address,
-  homeRoute = AppRoute.home,
-}) => {
-  const navigate = useNavigate();
-  const config = useSelectedNetworkConfig();
+                                               address,
+                                               homeRoute = AppRoute.home,
+                                           }) => {
+    const navigate = useNavigate();
+    const config = useSelectedNetworkConfig();
 
-  return (
-    <Body>
-      <Gap />
-        <img src="ion-logo.svg" alt="ION" width={117} height={117}/>
-        <Center>
-            <H1>Confirm</H1>
-            <Text>Transaction finished</Text>
-      </Center>
-      <ButtonColumn>
-        <ButtonNegative
-          onClick={() => {
-            ExtensionPlatform.openTab({
-              url: `${config.scanUrl}${address}`,
-            });
-          }}
-        >
-          View in explorer <LinkIcon />
-        </ButtonNegative>
-        <ButtonPositive
-          onClick={() => {
-            navigate(homeRoute);
-          }}
-        >
-          Close
-        </ButtonPositive>
-      </ButtonColumn>
-      <Gap />
-    </Body>
-  );
+    return (
+        <Body>
+            <Gap/>
+            <Center>
+                <img src="ion-logo.svg" alt="ION" width={117} height={117}/>
+                <H1>Confirm</H1>
+                <Text>Transaction finished</Text>
+            </Center>
+            <ButtonColumn>
+                <ButtonNegative
+                    onClick={() => {
+                        ExtensionPlatform.openTab({
+                            url: `${config.scanUrl}${address}`,
+                        });
+                    }}
+                >
+                    View in explorer <LinkIcon/>
+                </ButtonNegative>
+                <ButtonPositive
+                    onClick={() => {
+                        navigate(homeRoute);
+                    }}
+                >
+                    Close
+                </ButtonPositive>
+            </ButtonColumn>
+            <Gap/>
+        </Body>
+    );
 };
